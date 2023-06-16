a5e6bb9dd6804131b2613de578d1b39653ca829b
Fijiiiii <carljersonoquila13@gmail.com>
2023-06-16 20:08:38 +0800

    revision

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,7 +83,7 @@ $(document).ready(function() {
           
           var bottom_of_object = $(this).position().top + $(this).outerHeight();
           var bottom_of_window = $(window).scrollTop() + $(window).height();
-          if( bottom_of_window >= bottom_of_object ){
+          if( bottom_of_window <= bottom_of_object ){
               
               $(this).animate({'opacity':'100'},2000);
                   
